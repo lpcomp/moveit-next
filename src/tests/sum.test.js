0c3c778bf254fdfbe142d37a32c0486a955ba7a0
Luiz Paulo Baçal de Vasconcelos <luizv993@gmail.com>
2021-06-07 18:00:43 -0300

    jest and eslint

---
+++ src/tests/sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('../contexts/sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('teste do bom dia', () => {
+    expect('bom dia').toMatch(/bom/);
+});
